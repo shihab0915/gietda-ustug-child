5fa484a196876c9222c10a4de2d9824d25855338
Shihab <shihabdinajpur@gmail.com>
2018-03-05 19:38:37 +0600

    updating front-page, functions, header, mainScript, style

--- front-page.php
+++ front-page.php
@@ -95,15 +95,54 @@ get_header(); ?>
 <!-- Project Categories end-->
 
 <!-- HOW IT WORKS -->
-<section class="container-fluid well" style="margin-bottom: 30px;">
-  <h1 class="headline text-center">How it Works</h1>
-   <div class="col-sm-3">the cow</div>
-   <div class="col-sm-3">the dog</div>
-   <div class="col-sm-3">the man</div>
-   <div class="col-sm-3">the woman</div>
-    
+<section class="container-fluid well section-howitworks">
+<h1 class="headline headline--bold text-center how_it_works">How it Works</h1>
+    <div class="row col-container">
+        <div class="col-md-3 col-sm-6 find">
+            <div class="col">
+               <img class="center-block img-responsive" src="<?php echo get_theme_file_uri('/images/job-posting.png'); ?>" alt="job-posting">
+            </div>
+            <hr>
+           <h6>Find</h6>
+           <p class="text-center">
+               Create a free Employer or Freelancer account in minutes. Employers post jobs and Freelancers create profiles and submit quotes to open jobs. Employers review the quotes, communicate with the freelancers, and choose who they want to Hire.
+           </p>
+        </div>
+        <div class="col-md-3 col-sm-6 hire">
+           <div class="col">
+               <img class="center-block img-responsive" src="<?php echo get_theme_file_uri('/images/hiring.png'); ?>" alt="hiring">
+           </div>
+           <hr>
+           <h6>Hire</h6>
+           <p class="text-center">
+            Browse profiles, reviews, and proposals then interview top candidates. Hire a favorite and begin your project. 
+           </p>
+        </div>
+        <div class="col-md-3 col-sm-6 work">
+            <div class="col">
+               <img class="center-block img-responsive" src="<?php echo get_theme_file_uri('/images/work.png'); ?>" alt="working">
+            </div>
+            <hr>
+            <h6>Work</h6>
+            <p class="text-center">
+               Use the Gielda Uslug platform to messages, share files, and collaborate from your desktop or mobile.
+           </p>
+        </div>
+        <div class="col-md-3 col-sm-6 pay">
+            <div class="col">
+               <img class="center-block img-responsive" src="<?php echo get_theme_file_uri('/images/payment.png'); ?>" alt="pay-for-job">
+            </div>
+            <hr>
+            <h6>Pay</h6>
+            <p class="text-center">
+               All payments happen through Gielda Uslug. With Escrow Protection, only pay for work you authorize. 
+            </p>
+       </div>
+    </div>
 </section>
 <!-- HOW IT WORKS END -->
+
+
 
 <div class="container">
     <div class="row">
--- js/main.js
+++ js/main.js
@@ -16,6 +16,45 @@ jQuery(document).ready(function($){
         cat_holder.removeClass("ini-pos");
     }, {offset: '70%'});
     
+// FRONT PAGE HOW IT WORKS
+    var how = $('.how_it_works');
+    how.css('visibility', 'hidden');
+    how.waypoint(function() {
+        how.addClass("animated fadeInLeft");
+        how.css('visibility', 'visible');
+    }, {offset: '85%'});
+    
+// FRONT PAGE HOW IT WORKS- FIND
+    var find = $('.find');
+    find.css('visibility', 'hidden');
+    find.waypoint(function() {
+        find.addClass("animated slideInUp");
+        find.css('visibility', 'visible');
+    }, {offset: '70%'});
+    
+// FRONT PAGE HOW IT WORKS- HIRE
+    var hire = $('.hire');
+    hire.css('visibility', 'hidden');
+    hire.waypoint(function() {
+        hire.addClass("animated slideInUp");
+        hire.css('visibility', 'visible');
+    }, {offset: '70%'});  
+    
+// FRONT PAGE HOW IT WORKS- HIRE
+    var work = $('.work');
+    work.css('visibility', 'hidden');
+    work.waypoint(function() {
+        work.addClass("animated slideInUp");
+        work.css('visibility', 'visible');
+    }, {offset: '70%'}); 
+    
+// FRONT PAGE HOW IT WORKS- HIRE
+    var pay = $('.pay');
+    pay.css('visibility', 'hidden');
+    pay.waypoint(function() {
+        pay.addClass("animated slideInUp");
+        pay.css('visibility', 'visible');
+    }, {offset: '70%'});      
     
     
     
